7d780887a8472e8d5f6c7a01f4f49cb25e8c2577
Dima <mr.tarasiuk@gmail.com>
2023-07-19 09:31:15 +0200

    added skill controller

--- src/main/java/org/example/controller/SkillController.java
+++ src/main/java/org/example/controller/SkillController.java
@@ -3,6 +3,11 @@ package org.example.controller;
 import org.example.model.Skill;
 
 public class SkillController {
+
+    static final String JDBC_DRIVER =  "com.mysql.jdbc.Driver";
+    static final String DATABASE_URL = "jdbc:mysql://localhost:3306/";
+
+
     public void updateSkill(){
 
     }
